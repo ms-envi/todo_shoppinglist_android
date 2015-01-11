0da77136b53d7b72360eac1642a48664060c925c
envi <ms.envi@gmail.com>
2015-01-11 14:34:42 +0100

    TodoListActivity

--- todo_shoppinglist_app/gen/com/example/todo_shoppinglist_app/R.java
+++ todo_shoppinglist_app/gen/com/example/todo_shoppinglist_app/R.java
@@ -38,11 +38,11 @@ public final class R {
         public static final int action_settings=0x7f09000e;
         public static final int add=0x7f090007;
         public static final int add2=0x7f09000c;
-        public static final int autoCompleteTextView1=0x7f09000a;
         public static final int button1=0x7f090000;
         public static final int button2=0x7f090001;
         public static final int clear=0x7f090006;
         public static final int clear1=0x7f09000b;
+        public static final int editText2=0x7f09000a;
         public static final int food_category=0x7f090003;
         public static final int food_item=0x7f090004;
         public static final int listView1=0x7f090009;
--- todo_shoppinglist_app/src/com/example/todo_shoppinglist_app/TodoListActivity.java
+++ todo_shoppinglist_app/src/com/example/todo_shoppinglist_app/TodoListActivity.java
@@ -1,19 +1,81 @@
 package com.example.todo_shoppinglist_app;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class TodoListActivity extends Activity{
+public class TodoListActivity extends Activity {
 
-	
+	private EditText mAddText;
+	private Button mBtnAdd, mBtnClear;
+	private TextView mTitle;
+	private ListView mTodoListView;
+	private ArrayAdapter<String> todoListAdapter;
+	private ArrayList<String> todoItemsList;
+
+	private static final String TAG = "TodoListActivity";
+
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.todo_list);
-        
-     
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.todo_list);
+		Log.i(TAG, "onCreate called");
+
+		mAddText = (EditText) findViewById(R.id.editText2);
+		mBtnAdd = (Button) findViewById(R.id.add2);
+		mBtnClear = (Button) findViewById(R.id.clear1);
+		mTitle = (TextView) findViewById(R.id.textView2);
+		mTodoListView = (ListView) findViewById(R.id.listView1);
+
+		todoItemsList = new ArrayList<String>();
+		todoListAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, todoItemsList);
+
+		mBtnClear.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				mAddText.setText("");
+
+			}
+		});
+
+		mBtnAdd.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				if (mAddText.getText().toString().equals(""))
+					Toast.makeText(getApplicationContext(), "Field is empty",
+							Toast.LENGTH_LONG).show();
+				else {
+
+					addItem(mAddText.getText().toString());
+
+					mTodoListView.setAdapter(todoListAdapter);
+					mAddText.setText("");
+				}
+
+			}
+		});
+
+	}
+
+	public void addItem(String item) {
+
+		todoItemsList.add(item);
+		todoListAdapter.notifyDataSetChanged();
+
+	}
 }
